049c259818d8d96c92247c9edb32a1d4aceac51f
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2019-02-14 18:27:27 +0100

    added job_monitor

--- muimock/ui/__init__.py
+++ muimock/ui/__init__.py
@@ -1,4 +1,4 @@
 # noinspection PyUnresolvedReferences
 from .data import data_ui
 # noinspection PyUnresolvedReferences
-from .exec import exec_ui, Job, JobStatus, JobStatusList
+from .exec import exec_ui, job_monitor, Job, JobStatus, JobStatusList
