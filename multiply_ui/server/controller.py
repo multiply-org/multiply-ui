12702d397923bd0fb12097c853889f3a9d5b091e
thomas <thomas.storm@brockmann-consult.de>
2019-05-22 14:11:32 +0200

    added installation of json resource

--- multiply_ui/server/controller.py
+++ multiply_ui/server/controller.py
@@ -2,7 +2,7 @@ import json
 import pkg_resources
 
 def get_parameters(ctx):
-    json_text = pkg_resources.resource_string("multiply_ui", "server/resources/processing-parameters.json")
+    json_text = pkg_resources.resource_string(__name__, "resources/processing-parameters.json")
     return json.loads(json_text)
 
 def get_request(ctx, parameters):
