b0bbac39fd761339992aaed3a4eae9d08a66df6d
TonioF <tonio.fincke@brockmann-consult.de>
2019-05-30 17:49:54 +0100

    pep 8

--- multiply_ui/ui/job/form.py
+++ multiply_ui/ui/job/form.py
@@ -60,13 +60,13 @@ def obs_job_form(job: Job, mock=False):
     job_header_name_label = widgets.Label('Job Name')
     job_header_progress_label = widgets.Label('Progress')
     job_header_status_label = widgets.Label('Status')
-    job_progress = widgets.IntProgress(value=job.progress, min=0, max=100)
-    job_status = widgets.Label(job.status)
+    job_progress_bar = widgets.IntProgress(value=job.progress, min=0, max=100)
+    job_status_label = widgets.Label(job.status)
     job_id_label = widgets.Label(job.id)
     job_name_label = widgets.Label(job.name)
     job_grid_box = widgets.GridBox(children=[job_header_id_label, job_header_name_label,
                                             job_header_progress_label, job_header_status_label,
-                                            job_id_label, job_name_label, job_progress, job_status],
+                                            job_id_label, job_name_label, job_progress_bar, job_status_label],
                                     layout=widgets.Layout(
                                     width='50%',
                                     grid_template_rows='auto auto',
@@ -78,8 +78,8 @@ def obs_job_form(job: Job, mock=False):
     task_header_status_label = widgets.Label('Status')
     task_gridbox_children = [task_header_name_label, task_header_progress_label, task_header_status_label]
     task_gridbox_template_rows = 'auto'
-    progress_bars = []
-    status_labels = []
+    task_progress_bars = []
+    task_status_labels = []
     for task_name in job.tasks.names:
         progress = job.tasks.get(task_name).progress
         status = job.tasks.get(task_name).status
@@ -87,10 +87,11 @@ def obs_job_form(job: Job, mock=False):
         status_label = widgets.Label(status)
         task_name_label = widgets.Label(task_name)
         task_gridbox_children.append(task_name_label)
+        # noinspection PyTypeChecker
         task_gridbox_children.append(progress)
         task_gridbox_children.append(status_label)
-        progress_bars.append(progress)
-        status_labels.append(status_label)
+        task_progress_bars.append(progress)
+        task_status_labels.append(status_label)
         task_gridbox_template_rows = task_gridbox_template_rows + ' auto'
 
     task_grid_box = widgets.GridBox(children=task_gridbox_children,
@@ -105,19 +106,20 @@ def obs_job_form(job: Job, mock=False):
     def _update_job(job_state: Job):
         job.update(job_state.as_dict())
 
-    def monitor(job_progress, job_status, progress_bars, status_labels):
+    def monitor(progress_bar, status_bar, progress_bars, status_labels):
         while True:
-            job_progress.value = job.progress
-            job_status.value = job.status
-            task_names = job.tasks.names
-            for index, task_name in enumerate(task_names):
-                task = job.tasks.get(task_name)
+            progress_bar.value = job.progress
+            status_bar.value = job.status
+            job_task_names = job.tasks.names
+            for index, job_task_name in enumerate(job_task_names):
+                task = job.tasks.get(job_task_name)
                 progress_bars[index].value = task.progress
                 status_labels[index].value = task.status
             time.sleep(0.5)
             get_job_func(job, _update_job)
 
-    monitor_thread = threading.Thread(target=monitor, args=(job_progress, job_status, progress_bars, status_labels))
+    monitor_thread = threading.Thread(target=monitor, args=(job_progress_bar, job_status_label,
+                                                            task_progress_bars, task_status_labels))
     # noinspection PyTypeChecker
     display(job_monitor)
     monitor_thread.start()
