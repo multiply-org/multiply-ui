2181e84746f988fe541c97d69346ae5056fc0d49
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2019-06-17 11:49:59 +0200

    added address to config

--- multiply_ui/server/config.py
+++ multiply_ui/server/config.py
@@ -1,5 +1,6 @@
 import logging
 
+ADDRESS = '0.0.0.0'
 PORT = 9090
 # noinspection SpellCheckingInspection
 SUPPORTED_VARIABLES = ['lai', 'cab', 'cb', 'car', 'cw', 'cdm', 'n', 'ala', 'h', 'bsoil', 'psoil']
