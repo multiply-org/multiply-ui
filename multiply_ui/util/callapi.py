47e0d576b2354653b14508cfea2a458119955c52
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2019-07-08 15:14:48 +0200

    use requests package

--- multiply_ui/util/callapi.py
+++ multiply_ui/util/callapi.py
@@ -1,6 +1,5 @@
 import json
-import urllib.error
-import urllib.request
+import requests
 from typing import Any
 
 
@@ -14,15 +13,13 @@ def call_api(url: str, apply_func=None, data=None) -> Any:
     :return: response JSON object, usually a dictionary.
     """
     try:
-        # TODO: use a truly asynchronous client, e.g. Python package "requests".
-        with urllib.request.urlopen(url, data=data) as response:
-            json_obj = json.loads(response.read())
-            return apply_func(json_obj) if apply_func is not None else json_obj
-    except urllib.error.HTTPError as e:
-        # TODO: add error handler
-        print(f"Server error: {e}")
-        return None
-    except urllib.error.URLError as e:
+        if data is None:
+            response = requests.get(url)
+        else:
+            response = requests.post(url, json=data)
+        json_obj = json.loads(response.content)
+        return apply_func(json_obj) if apply_func is not None else json_obj
+    except Exception as e:
         # TODO: add error handler
         print(f"Connection error: {e}")
         return None
